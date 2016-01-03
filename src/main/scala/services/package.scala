f0dd429fa5c37e5c6cf7cfcd00ed9d0df157549c
Vadim Bondarev <haghard84@gmail.com>
2016-01-03 14:00:42 +0300

    Improve services example

--- src/main/scala/services/package.scala
+++ src/main/scala/services/package.scala
@@ -4,24 +4,25 @@ package object services {
   import scalaz.concurrent.Task
   import scalaz._, Scalaz._
 
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  implicit val FutureMonad = new Monad[Future] {
-    def point[A](a: => A): Future[A] = Future(a)
+  /*
+  implicit def FutureMonad(implicit ctx: scala.concurrent.ExecutionContext) = new Monad[Future] {
+    def point[A](a: => A): Future[A] = Future(a)(ctx)
     def bind[A, B](fa: Future[A])(f: (A) => Future[B]): Future[B] = fa flatMap f
-  }
+  }*/
 
   trait TwitterModule[M[_]] {
     type Tweet
-    type TwitterApi <: TwitterApiLike[Tweet]
-    type Result = ValidationNel[String, Tweet]
+    type TwitterContext
+    type TwitterApi <: TwitterApiLike
+    type ValidTweet = ValidationNel[String, Tweet]
 
+    implicit def TwitterCtx: TwitterContext
     def twitterEffect: scalaz.Monad[M]
 
-    protected trait TwitterApiLike[T] {
-      def load(query: String): M[Result]
-      def reduce(query: String): M[Result]
-      protected def take(n: Int): List[T]
+    protected trait TwitterApiLike {
+      def load(query: String): M[ValidTweet]
+      def reduce(query: String): M[ValidTweet]
+      protected def take(n: Int): List[Tweet]
     }
 
     def twitterApi: TwitterApi
@@ -29,14 +30,16 @@ package object services {
 
   trait DbServiceModule[M[_]] {
     type Record
-    type DbResult = ValidationNel[String, Record]
-    type DbApi <: DbApiLike[Record]
+    type DbContext
+    type DbApi <: DbApiLike
+    type ValidRecord = ValidationNel[String, Record]
 
+    implicit def DbCtx: DbContext
     def dbEffect: scalaz.Monad[M]
 
-    protected trait DbApiLike[T] {
-      def one(query: String): M[DbResult]
-      def page(query: String): M[DbResult]
+    protected trait DbApiLike {
+      def one(query: String): M[ValidRecord]
+      def page(query: String): M[ValidRecord]
     }
 
     def dbApi: DbApi
@@ -44,15 +47,19 @@ package object services {
 
   trait ScalazFutureTwitter extends TwitterModule[Future] {
     override type Tweet = Int
+    override type TwitterContext = scala.concurrent.ExecutionContext
     override type TwitterApi = ScalazFutureApi
     override val twitterEffect: scalaz.Monad[Future] = scalaz.Monad[Future]
 
-    final class ScalazFutureApi extends TwitterApiLike[Tweet] {
-      override def load(query: String): Future[Result] =
-        Future(0.successNel[String])
+    override implicit val TwitterCtx = scala.concurrent.ExecutionContext.Implicits.global
 
-      override def reduce(query: String): Future[Result] =
-        Future(take(5).foldMap(i => i)(implicitly[Monoid[Tweet]]).successNel[String])
+    final class ScalazFutureApi extends TwitterApiLike {
+
+      override def load(query: String): Future[ValidTweet] =
+        Future(0.successNel[String])(TwitterCtx)
+
+      override def reduce(query: String): Future[ValidTweet] =
+        Future(take(5).foldMap(i => i)(implicitly[Monoid[Tweet]]).successNel[String])(TwitterCtx)
 
       protected def take(n: Int): List[Tweet] = List.range(0, n)
     }
@@ -60,13 +67,41 @@ package object services {
     override lazy val twitterApi = new ScalazFutureApi()
   }
 
+  trait ScalazTaskTwitter extends TwitterModule[Task] {
+    override type Tweet = Int
+    override type TwitterContext = java.util.concurrent.Executor //scalaz.concurrent.Strategy
+    override type TwitterApi = TwitterApiLike
+    override val twitterEffect: scalaz.Monad[Task] = scalaz.Monad[Task]
+
+    override implicit val TwitterCtx = java.util.concurrent.Executors.newFixedThreadPool(2)
+
+    final class ScalazTaskApi extends TwitterApiLike {
+
+      override def load(query: String): Task[ValidTweet] = Task(0.successNel[String])
+
+      override def reduce(query: String): Task[ValidTweet] =
+        Task {
+          (1.successNel[String] :: 2.successNel[String] :: "3 error".failureNel[Tweet] :: "4 error".failureNel[Tweet] :: Nil).sequenceU
+          .map(_.foldMap(t=>t)(implicitly[Monoid[Tweet]]))
+        }(TwitterCtx)
+
+      override protected def take(n: Int): List[Tweet] = List.range(0, n)
+    }
+
+    override lazy val twitterApi = new ScalazTaskApi()
+  }
+
   trait ScalazFutureDbService extends DbServiceModule[Future] {
     override type Record = Int
-    override val dbEffect: scalaz.Monad[Future] = scalaz.Monad[Future]
+    override type DbContext = scala.concurrent.ExecutionContext
     override type DbApi = ScalazFutureApi
 
-    final class ScalazFutureApi extends DbApiLike[Record] {
-      override def one(query: String) = {
+    override implicit val DbCtx = scala.concurrent.ExecutionContext.Implicits.global
+    override val dbEffect: scalaz.Monad[Future] = scalaz.Monad[Future]
+
+    final class ScalazFutureApi extends DbApiLike {
+
+      override def one(query: String) =
         Future {
           Thread.sleep(200)
           println(s"one:start ${Thread.currentThread().getName}")
@@ -77,18 +112,17 @@ package object services {
         }.recover {
           case ex: Throwable => ex.getMessage.failureNel[Record]
         }
-      }
 
-      override def page(query: String): Future[DbResult] =
+      override def page(query: String): Future[ValidRecord] =
         Future {
           Thread.sleep(500)
           println(s"page:start ${Thread.currentThread().getName}")
           Thread.sleep(300)
           println(s"page:end ${Thread.currentThread().getName}")
           //throw new Exception("page error")
-          List(1, 2, 3, 4, 5)
-        }.map(list => (list.foldMap(i => i)(implicitly[Monoid[Record]])).successNel[String])
-        .recover {
+          (1.successNel[String] :: 2.successNel[String] :: "3 error".failureNel[Record] :: "4 error".failureNel[Record] :: Nil).sequenceU
+            .map(_.foldMap(i=>i)(implicitly[Monoid[Record]]))
+        }.recover {
           case ex: Throwable => ex.getMessage.failureNel[Record]
         }
     }
@@ -98,24 +132,24 @@ package object services {
 
   trait ScalazTaskDbService extends DbServiceModule[Task] {
     override type Record = Int
+    override type DbContext = java.util.concurrent.Executor //scalaz.concurrent.Strategy
     override val dbEffect: scalaz.Monad[Task] = scalaz.Monad[Task]
     override type DbApi = ScalazTaskApi
 
-    final class ScalazTaskApi extends DbApiLike[Record] {
+    implicit val DbCtx = java.util.concurrent.Executors.newFixedThreadPool(2)
+
+    final class ScalazTaskApi extends DbApiLike {
       override def one(query: String) =
-        Task(0).attemptRun match {
+        Task(0)(DbCtx).attemptRun match {
           case \/-(r) => Task.now(r.successNel[String])
           case -\/(ex) => Task.now(ex.getMessage.failureNel[Record])
         }
 
-      override def page(query: String): Task[DbResult] = {
+      override def page(query: String): Task[ValidRecord] = {
         Task {
-          (10.successNel[String] :: 11.successNel[String] ::
-            "12.fetch error".failureNel[Record] ::
-            "13 fetch error".failureNel[Record] :: Nil)
-             .sequenceU
+          (10.successNel[String] :: 11.successNel[String] :: "12 error".failureNel[Record] :: "13 error".failureNel[Record] :: Nil).sequenceU
             .map(_.foldMap(i=>i)(implicitly[Monoid[Record]]))
-        }
+        }(DbCtx)
       }
     }
 
@@ -123,18 +157,35 @@ package object services {
   }
 
   object FutureDbService extends ScalazFutureDbService
+  object ScalazFutureTwitterService extends ScalazFutureTwitter
+
   object TaskDbService extends ScalazTaskDbService
+  object ScalazTaskTwitterService extends ScalazTaskTwitter
 
   object ApplicationFutureService extends ScalazFutureTwitter with ScalazFutureDbService {
+
+    def zip =
+      ((twitterApi reduce "select page") zip (dbApi page "select page"))
+        .map(pair => (pair._1 |@| pair._2) {case (a, b) ⇒ s"twitter:$a db:$b"})(TwitterCtx)
+
+    def zip2 = {
+      (twitterApi reduce "select page").flatMap { a =>
+        (dbApi page "select page").map { b =>
+          ((a |@| b) { case (a, b) ⇒ s"twitter:$a db:$b" })
+        }(DbCtx)
+      }(TwitterCtx)
+    }
+  }
+
+  object ApplicationTaskService extends ScalazTaskTwitter with ScalazTaskDbService {
     def zip = for {
-      x <- twitterApi.reduce("select page")
-      y <- dbApi.page("select page")
-    } yield ((x |@| y){case (a, b) ⇒ s"twitterApi:$a dbApi:$b"})
+      x <- (twitterApi reduce "select page")
+      y <- (dbApi page "select page")
+    } yield ((x |@| y){case (a, b) ⇒ s"twitter:$a db:$b"})
   }
 
 /*
 
-  import scala.concurrent.ExecutionContext.Implicits.global
   import services._
   import scalaz._, Scalaz._
 
