c0241496dcfaa0ddfe0bbe4aec96c7856928309a
Vadim Bondarev <haghard84@gmail.com>
2016-04-12 10:34:22 +0300

    Add TimedGate from examples

--- src/main/scala/recipes/AkkaRecipes.scala
+++ src/main/scala/recipes/AkkaRecipes.scala
@@ -1590,3 +1590,31 @@ class DuplicatorN[A] extends GraphStage[FlowShape[A, A]] {
       })
     }
 }
+
+class TimedGate[A](silencePeriod: FiniteDuration) extends GraphStage[FlowShape[A, A]] {
+  val in = Inlet[A]("TimedGate.in")
+  val out = Outlet[A]("TimedGate.out")
+  val shape = FlowShape.of(in, out)
+
+  override def createLogic(inheritedAttributes: Attributes) = new TimerGraphStageLogic(shape) {
+      var open = false
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val elem = grab(in)
+          if (open) pull(in)
+          else {
+            push(out, elem)
+            open = true
+            scheduleOnce(None, silencePeriod)
+          }
+        }
+      })
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = pull(in)
+      })
+
+      override protected def onTimer(timerKey: Any): Unit = {
+        open = false
+      }
+    }
+}
