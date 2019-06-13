bacef02c38c84f97d70f9ef053a943ea112edeec
haghard <haghard84@gmail.com>
2019-06-13 20:20:39 +0300

    fs2 1.0.5

--- src/main/scala/recipes/CustomStages.scala
+++ src/main/scala/recipes/CustomStages.scala
@@ -9,6 +9,7 @@ import akka.stream.stage.GraphStageLogic.StageActor
 
 import scala.collection.{ immutable, mutable }
 import akka.stream.stage._
+import akka.util.ByteString
 
 import scala.concurrent.duration.{ Duration, FiniteDuration }
 import scala.reflect.ClassTag
@@ -622,4 +623,51 @@ object CustomStages {
         }
       }
   }
+
+  /*
+    Flow.fromGraph(new Chunker(chunkSize))
+  */
+  class Chunker(val chunkSize: Int) extends GraphStage[FlowShape[ByteString, ByteString]] {
+    val in = Inlet[ByteString]("chunker.in")
+    val out = Outlet[ByteString]("chunker.out")
+
+    override val shape = FlowShape.of(in, out)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      private var buffer = ByteString.empty
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit =
+          //println("Buffer size: " + buffer.size)
+          emitChunk()
+      })
+
+      setHandler(
+        in,
+        new InHandler {
+          override def onPush(): Unit = {
+            val elem = grab(in)
+            buffer ++= elem
+            emitChunk()
+          }
+
+          override def onUpstreamFinish(): Unit =
+            if (buffer.isEmpty) completeStage()
+            else {
+              if (isAvailable(out)) emitChunk()
+            }
+        })
+
+      private def emitChunk(): Unit =
+        if (buffer.isEmpty) {
+          if (isClosed(in)) completeStage()
+          else pull(in)
+        } else {
+          val (chunk, nextBuffer) = buffer.splitAt(chunkSize)
+          buffer = nextBuffer
+          push(out, chunk)
+        }
+    }
+  }
+
 }
