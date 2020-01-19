d3631c0afe5be9c53e10cf17ac5be75d89292212
haghard <haghard84@gmail.com>
2020-01-19 23:50:15 +0100

    more examples

---
+++ src/main/scala/recipes/ws/WebSocketClient.scala
@@ -0,0 +1,129 @@
+package recipes.ws
+
+import java.util.concurrent.ThreadLocalRandom
+
+import akka.stream.{ActorMaterializer, FlowShape, Graph, KillSwitches, SinkShape, SourceShape}
+import akka.actor.{ActorRef, ActorSystem}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest, WebSocketUpgradeResponse}
+import akka.stream.scaladsl.{Flow, GraphDSL, Keep, Sink, Source}
+import recipes.ws.WindTurbineSimulator.{FailedUpgrade, Upgraded}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.util.{Failure, Random, Success}
+
+object WindTurbineData {
+
+  def apply(id: String) = new WindTurbineData(id)
+}
+
+class WindTurbineData(id: String) {
+  val random = Random
+
+
+  def getNext: String = {
+    val timestamp  = System.currentTimeMillis / 1000
+    val power      = f"${ThreadLocalRandom.current.nextDouble * 10}%.2f"
+    val rotorSpeed = f"${ThreadLocalRandom.current.nextDouble * 10}%.2f"
+    val windSpeed  = f"${ThreadLocalRandom.current.nextDouble * 100}%.2f"
+
+    s"""{
+       |    "id": "$id",
+       |    "timestamp": $timestamp,
+       |    "measurements": {
+       |        "power": $power,
+       |        "rotor_speed": $rotorSpeed,
+       |        "wind_speed": $windSpeed
+       |    }
+       |}""".stripMargin
+  }
+}
+
+object WebSocketClient {
+
+  def apply(
+    id: String,
+    endpoint: String,
+    supervisor: ActorRef
+  )(implicit sys: ActorSystem, mat: ActorMaterializer, ec: ExecutionContext) =
+    new WebSocketClient(id, endpoint, supervisor)(sys, mat, ec)
+
+}
+
+class WebSocketClient(id: String, endpoint: String, supervisor: ActorRef)(
+  implicit
+  system: ActorSystem,
+  materializer: ActorMaterializer,
+  executionContext: ExecutionContext) {
+
+  val webSocket: Flow[Message, Message, Future[WebSocketUpgradeResponse]] = {
+    val websocketUri = s"$endpoint/$id"
+    Http().cachedHostConnectionPool()
+    Http().webSocketClientFlow(WebSocketRequest(websocketUri))
+  }
+
+  //Streams telemetry to the service, once a second.
+  val outgoing: Graph[SourceShape[Message], akka.NotUsed] = GraphDSL.create() { implicit b ⇒
+    val data = WindTurbineData(id)
+
+    val flow = b.add {
+      Source
+        .tick(1.seconds, 1.seconds, ())
+        .map(_ ⇒ TextMessage(data.getNext))
+    }
+
+    SourceShape(flow.out)
+  }
+
+  //Life-cycle messages from server
+  //Graph[SinkShape[Message], akka.NotUsed]
+  val incoming: Graph[FlowShape[Message, Unit], akka.NotUsed] = GraphDSL.create() { implicit b ⇒
+    val flow = b.add {
+      Flow[Message]
+        .collect {
+          case TextMessage.Strict(text) ⇒
+            Future.successful(text)
+          case TextMessage.Streamed(textStream) ⇒
+            textStream
+              .runFold("")(_ + _)
+              .flatMap(Future.successful)
+        }
+        .mapAsync(1)(identity)
+        .map(println)
+    }
+
+    //akka.stream.SinkShape(flow.in)
+    FlowShape(flow.in, flow.out)
+  }
+
+  /*Flow
+    .fromSinkAndSourceCoupled(incoming, outgoing)
+    .viaMat(KillSwitches.single)(Keep.right)*/
+
+  val ((upgradeResponse, killSwitch), closed) =
+    Source
+      .fromGraph(outgoing)
+      .viaMat(webSocket)(Keep.right)          // keep the materialized Future[WebSocketUpgradeResponse]
+      .viaMat(KillSwitches.single)(Keep.both) // also keep the KillSwitch
+      .via(incoming)
+      .toMat(Sink.ignore)(Keep.both) // also keep the Future[Done]
+      .run()
+
+  upgradeResponse
+    .map { upgrade ⇒
+      upgrade.response.status match {
+        case StatusCodes.SwitchingProtocols ⇒ supervisor ! Upgraded
+        case statusCode                     ⇒ supervisor ! FailedUpgrade(statusCode)
+      }
+    }
+    .onComplete {
+      case Success(_)  ⇒ supervisor ! WindTurbineSimulator.Connected
+      case Failure(ex) ⇒ supervisor ! WindTurbineSimulator.ConnectionFailure(ex)
+    }
+
+  closed.map { _ ⇒
+    supervisor ! WindTurbineSimulator.Terminated
+  }
+}
---
+++ src/main/scala/recipes/ws/WindTurbineSimulator.scala
@@ -0,0 +1,58 @@
+package recipes.ws
+
+import WindTurbineSimulator._
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.model.StatusCode
+import akka.actor.{Actor, ActorLogging, Props}
+
+//https://blog.colinbreck.com/integrating-akka-streams-and-akka-actors-part-ii/
+
+//The following actor simulates a wind turbine
+object WindTurbineSimulator {
+
+  def props(id: String, endpoint: String)(implicit materializer: ActorMaterializer) =
+    Props(classOf[WindTurbineSimulator], id, endpoint, materializer)
+
+  final case object Upgraded
+  final case object Connected
+  final case object Terminated
+  final case class ConnectionFailure(ex: Throwable)
+  final case class FailedUpgrade(statusCode: StatusCode)
+
+  case class WindTurbineSimulatorException(id: String) extends Exception(s"Turbine:$id error")
+
+}
+
+class WindTurbineSimulator(id: String, endpoint: String)(implicit mat: ActorMaterializer) extends Actor
+    with ActorLogging {
+  implicit val system           = context.system
+  implicit val ec = system.dispatcher
+
+  //creates a WebSocket connection
+  val webSocket = WebSocketClient(id, endpoint, self)
+
+  override def postStop() = {
+    log.info(s"$id : Stopping WebSocket connection")
+    webSocket.killSwitch.shutdown()
+  }
+
+  override def receive: Receive = {
+    case Upgraded ⇒
+      log.info(s"$id : WebSocket upgraded")
+    case FailedUpgrade(statusCode) ⇒
+      log.error(s"$id : Failed to upgrade WebSocket connection : $statusCode")
+      throw WindTurbineSimulatorException(id)
+    case ConnectionFailure(ex) ⇒
+      log.error(s"$id : Failed to establish WebSocket connection $ex")
+      throw WindTurbineSimulatorException(id)
+    case Connected ⇒
+      log.info(s"$id : WebSocket connected")
+      context become active
+  }
+
+  def active: Receive = {
+    case Terminated ⇒
+      log.error(s"$id : WebSocket connection terminated")
+      throw WindTurbineSimulatorException(id)
+  }
+}
